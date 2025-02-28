409acd57fa85f8bc785b4be522565e23ec6adab8
akiritsa <akbcm98@gmail.com>
2025-03-01 00:09:09 +0300

    Update leet_go_two_sum.go

--- leet_go_two_sum.go
+++ leet_go_two_sum.go
@@ -1,4 +1,4 @@
-package Practise
+package main
 
 import "fmt"
 
@@ -6,19 +6,19 @@ var count int = 0
 
 func main() {
 
-	Nums := []int{10, 2, 3, 4, 5}
+	Nums := []int{10, 2, 7, 4, 5}
 	Target := 5
 
 	fmt.Println(twoSum(Nums, Target), "Итераций: ", count)
 }
 func twoSum(nums []int, target int) []int {
-	var Output []int
+	//	var Output []int
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			count++
 			if nums[i]+nums[j] == target {
-				Output = []int{nums[i], nums[j]}
-				return Output
+				//				Output = []int{nums[i], nums[j]}
+				return []int{i, j}
 			}
 		}
 	}
